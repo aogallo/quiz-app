001363544b491aeade310030e2d27e77e59165a6
Allan Gallo <allan.gallo.guerra@gmail.com>
2023-11-14 18:03:13 -0600

    add error to useApi hook

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -4,18 +4,25 @@ import { QuestionType } from '../screens/QuestionScreen'
 
 const useApi = () => {
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
   const [questions, setQuestions] = useState<QuestionType[]>([])
 
   useEffect(() => {
-    getQuestions().then((data) => {
-      setQuestions(data)
-      setIsLoading(false)
-    })
+    getQuestions()
+      .then((data) => {
+        setQuestions(data)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        setError(error)
+      })
   }, [])
 
   return {
     questions,
     isLoading,
+    error,
   }
 }
 
